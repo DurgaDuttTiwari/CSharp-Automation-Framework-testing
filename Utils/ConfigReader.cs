529c1be71bd864989c0c18242c2902491035403e
DurgaDuttTiwari <durgadutt416@gmail.com>
2025-04-27 17:02:27 +0530

    changed the secreat name and updated code accordingly

--- Utils/ConfigReader.cs
+++ Utils/ConfigReader.cs
@@ -1,31 +1,52 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace ReqnrollFirstTestProject.Utils
 {
     public static class ConfigReader
     {
-        private static readonly JObject config;
+        private static readonly JObject? config;
 
         static ConfigReader()
         {
             var configPath = Path.Combine(Directory.GetCurrentDirectory(), "UserDetails.json");
-            var json = File.ReadAllText(configPath);
-            config = JObject.Parse(json);
+
+            if (File.Exists(configPath))
+            {
+                var json = File.ReadAllText(configPath);
+                config = JObject.Parse(json);
+            }
+            else
+            {
+                config = null; // No local file, will use environment variables
+            }
         }
 
         public static string GetEmail()
         {
-            return config["email"]?.ToString();
+            // 1. Priority: Environment Variable (for GitHub Actions)
+            var emailFromEnv = Environment.GetEnvironmentVariable("TEST_EMAIL");
+            if (!string.IsNullOrEmpty(emailFromEnv))
+                return emailFromEnv;
+
+            // 2. Priority: Local config file
+            if (config?["email"] != null)
+                return config["email"]!.ToString();
+
+            throw new Exception("Email not found in environment variables or local config file.");
         }
 
         public static string GetPassword()
         {
-            return config["password"]?.ToString();
+            var passwordFromEnv = Environment.GetEnvironmentVariable("TEST_PASSWORD");
+            if (!string.IsNullOrEmpty(passwordFromEnv))
+                return passwordFromEnv;
+
+            if (config?["password"] != null)
+                return config["password"]!.ToString();
+
+            throw new Exception("Password not found in environment variables or local config file.");
         }
     }
 }
